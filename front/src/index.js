d13d692e2142db46ac273a458abc5113b5022b91
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-05-25 20:28:11 -0300

    10

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppFunc from "./app";
-import "./index.css"; // Optional: If you use Tailwind or global styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppFunc />);
