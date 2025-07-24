95aee8f61b5499e046364ec72dcbd17520c74af5
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-24 15:28:11 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "connections_table_name" {
   description = "DynamoDB table for storing WS connection IDs"
   type        = string
