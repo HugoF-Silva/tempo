9e27f79a028111c466297084b395dd3c798b4af3
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-24 16:14:31 -0300

    Update main.tf

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# pull in the bootstrap outputs
 data "terraform_remote_state" "bootstrap" {
   backend = "local"
   config = {
@@ -10,7 +9,6 @@ data "terraform_remote_state" "bootstrap" {
   }
 }
 
-# now reference (don't recreate) the table & roles
 data "aws_dynamodb_table" "connections" {
   name = data.terraform_remote_state.bootstrap.outputs.connections_table_name
 }
@@ -23,24 +21,17 @@ data "aws_iam_role" "bcast" {
   name = data.terraform_remote_state.bootstrap.outputs.bcast_lambda_role_name
 }
 
-# ----------------------------------------
-# Declare your WebSocket API
-# ----------------------------------------
 resource "aws_apigatewayv2_api" "websocket" {
   name                       = "healthCentersWS"
   protocol_type              = "WEBSOCKET"
   route_selection_expression = "$request.body.action"
 }
 
-# ----------------------------------------
-# Your Lambdas
-# ----------------------------------------
 resource "aws_lambda_function" "connect" {
   function_name    = "ws_connect_handler"
   role             = data.aws_iam_role.conn.arn
   handler          = "connect.handler"
   runtime          = var.lambda_runtime
-
   filename         = "connect.zip"
   source_code_hash = filebase64sha256("connect.zip")
 
@@ -56,7 +47,6 @@ resource "aws_lambda_function" "broadcast" {
   role             = data.aws_iam_role.bcast.arn
   handler          = "broadcast.handler"
   runtime          = var.lambda_runtime
-
   filename         = "broadcast.zip"
   source_code_hash = filebase64sha256("broadcast.zip")
 
@@ -67,3 +57,5 @@ resource "aws_lambda_function" "broadcast" {
     }
   }
 }
+
+# ... integrations, routes, stages, and CloudWatch rules go here ...
