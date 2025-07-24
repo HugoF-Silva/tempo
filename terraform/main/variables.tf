7100a8f7ded7e1c6a0de038aabb1b05816694b52
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-24 16:16:28 -0300

    Update variables.tf

--- terraform/main/variables.tf
+++ terraform/main/variables.tf
@@ -2,3 +2,9 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "lambda_runtime" {
+  description = "Runtime for all Lambda functions"
+  type        = string
+  default     = "nodejs18.x"
+}
