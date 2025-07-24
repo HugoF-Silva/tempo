9d7c3dfe4b663bc8f98eac7f80f88981b0de2000
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-24 16:41:56 -0300

    Update main.tf

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,85 +1,81 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.4.0"
-    }
-  }
+# ------------------------------------------------------------
+# 1. Replace your existing resource "aws_lambda_function.connect"
+#    with a data source that looks up the already‑deployed Lambda:
+# ------------------------------------------------------------
+data "aws_lambda_function" "connect" {
+  function_name = var.conn_lambda_function_name
 }
 
-provider "aws" {
-  region = var.aws_region
+# (Optionally do the same for broadcast if it, too, already exists)
+# data "aws_lambda_function" "broadcast" {
+#   function_name = var.bcast_lambda_function_name
+# }
+
+# ------------------------------------------------------------
+# 2. Create the integration pointing at that existing Lambda:
+# ------------------------------------------------------------
+resource "aws_apigatewayv2_integration" "connect_integration" {
+  api_id             = aws_apigatewayv2_api.websocket.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = data.aws_lambda_function.connect.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
 }
 
-# Pull in your _existing_ resources by name
-data "aws_dynamodb_table" "connections" {
-  name = var.connections_table_name
+# (And similarly for broadcast)
+# resource "aws_apigatewayv2_integration" "broadcast_integration" {
+#   api_id           = aws_apigatewayv2_api.websocket.id
+#   integration_type = "AWS_PROXY"
+#   integration_uri  = data.aws_lambda_function.broadcast.invoke_arn
+#   integration_method = "POST"
+#   payload_format_version = "2.0"
+# }
+
+# ------------------------------------------------------------
+# 3. Wire up your routes to use those integrations:
+# ------------------------------------------------------------
+resource "aws_apigatewayv2_route" "connect_route" {
+  api_id    = aws_apigatewayv2_api.websocket.id
+  route_key = "$connect"
+  target    = "integrations/${aws_apigatewayv2_integration.connect_integration.id}"
 }
 
-data "aws_iam_role" "conn" {
-  name = var.conn_lambda_role_name
+resource "aws_apigatewayv2_route" "disconnect_route" {
+  api_id    = aws_apigatewayv2_api.websocket.id
+  route_key = "$disconnect"
+  target    = "integrations/${aws_apigatewayv2_integration.connect_integration.id}"
 }
 
-
-data "aws_iam_role" "bcast" {
-  name = var.bcast_lambda_role_name
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.websocket.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.broadcast_integration.id}"
 }
 
-# Create (or update) the WebSocket API
-resource "aws_apigatewayv2_api" "websocket" {
-  name                       = "healthCentersWS"
-  protocol_type              = "WEBSOCKET"
-  route_selection_expression = "$request.body.action"
+# ------------------------------------------------------------
+# 4. Make sure API Gateway can invoke your Lambda
+# ------------------------------------------------------------
+resource "aws_lambda_permission" "allow_apigw_connect" {
+  statement_id  = "AllowExecutionFromAPIGatewayConnect"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.connect.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.websocket.execution_arn}/*/@connect"
 }
 
-# Bundle Lambdas on the fly
-data "archive_file" "connect" {
-  type        = "zip"
-  source_dir  = "../../lambda/connect"
-  output_path = "../../connect.zip"
+resource "aws_lambda_permission" "allow_apigw_default" {
+  statement_id  = "AllowExecutionFromAPIGatewayDefault"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.broadcast.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.websocket.execution_arn}/*/*"
 }
 
-data "archive_file" "broadcast" {
-  type        = "zip"
-  source_dir  = "../../lambda/broadcast"
-  output_path = "../../broadcast.zip"
+# ------------------------------------------------------------
+# 5. Deploy a stage (if you havent already)
+# ------------------------------------------------------------
+resource "aws_apigatewayv2_stage" "prod" {
+  api_id      = aws_apigatewayv2_api.websocket.id
+  name        = "prod"
+  auto_deploy = true
 }
-
-# $connect handler
-resource "aws_lambda_function" "connect" {
-  function_name    = "ws_connect_handler"
-  role             = data.aws_iam_role.conn.arn
-  handler          = "connect.handler"
-  runtime          = var.lambda_runtime
-  filename         = data.archive_file.connect.output_path
-  source_code_hash = data.archive_file.connect.output_base64sha256
-
-  environment {
-    variables = {
-      CONNECTIONS_TABLE = data.aws_dynamodb_table.connections.name
-    }
-  }
-}
-
-# broadcast handler
-resource "aws_lambda_function" "broadcast" {
-  function_name    = "ws_broadcast_handler"
-  role             = data.aws_iam_role.bcast.arn
-  handler          = "broadcast.handler"
-  runtime          = var.lambda_runtime
-  filename         = data.archive_file.broadcast.output_path
-  source_code_hash = data.archive_file.broadcast.output_base64sha256
-
-  environment {
-    variables = {
-      CONNECTIONS_TABLE = data.aws_dynamodb_table.connections.name
-      WS_API_ID         = aws_apigatewayv2_api.websocket.id
-    }
-  }
-}
-
-# …now add your integrations, routes, stages, CloudWatch rule, etc…
