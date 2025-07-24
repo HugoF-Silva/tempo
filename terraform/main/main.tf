7a514a57eb16c5981a8035d9e180ba1ff78be34a
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-24 16:08:11 -0300

    Update main.tf

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,11 +1,47 @@
-output "connections_table_name" {
-  value = aws_dynamodb_table.connections.name
+provider "aws" {
+  region = var.aws_region
 }
 
-output "conn_lambda_role_name" {
-  value = aws_iam_role.conn_lambda_role.name
+# pull in the bootstrap outputs
+data "terraform_remote_state" "bootstrap" {
+  backend = "local"
+  config = {
+    path = "../bootstrap/terraform.tfstate"
+  }
 }
 
-output "bcast_lambda_role_name" {
-  value = aws_iam_role.bcast_lambda_role.name
+# now reference (don't recreate) the table & roles
+data "aws_dynamodb_table" "connections" {
+  name = data.terraform_remote_state.bootstrap.outputs.connections_table_name
 }
+
+data "aws_iam_role" "conn" {
+  name = data.terraform_remote_state.bootstrap.outputs.conn_lambda_role_name
+}
+
+data "aws_iam_role" "bcast" {
+  name = data.terraform_remote_state.bootstrap.outputs.bcast_lambda_role_name
+}
+
+# (your existing websocket API + integrations here…)
+
+# when you define your Lambdas, swap in the data roles:
+resource "aws_lambda_function" "connect" {
+  # …
+  role = data.aws_iam_role.conn.arn
+  # …
+}
+
+resource "aws_lambda_function" "broadcast" {
+  # …
+  role = data.aws_iam_role.bcast.arn
+
+  environment {
+    variables = {
+      CONNECTIONS_TABLE = data.aws_dynamodb_table.connections.name
+      WS_API_ID         = aws_apigatewayv2_api.websocket.id
+    }
+  }
+}
+
+# (rest of your API Gateway, CloudWatch rule, etc.)
